04e9823a073887fb631efaccd2418056272bc1f9
五盏 <1779374060@qq.com>
2024-07-12 01:25:26 +0800

    markdown

--- fcweb/src/coms/blog/BlogContentCom.vue
+++ fcweb/src/coms/blog/BlogContentCom.vue
@@ -3,12 +3,29 @@
   import 'vditor/dist/index.css';
   import {onMounted, ref} from "vue";
 
-  const vditor = ref()
   onMounted(()=>{
-    vditor.value = new Vditor('vditor',{
-
-    })
+    vditor.value = new Vditor('vditor',options.value)
   })
+
+  /**
+   * 编辑器设置
+   */
+  const vditor = ref()
+  const editing = ref(false)
+  const options = ref({
+    mode: editing.value ? 'sv' : 'ir',
+    toolbar:[
+       'emoji' , 'upload', '|' , 'line' , 'ordered-list' , 'check' , 'code' , 'link' , 'table', '|' , 'outline' , 'edit-mode', 'fullscreen' , 'export',  'br', '|', 'undo' , 'redo' ,
+    ],
+    preview:{
+      maxWidth: 1000
+    },
+    toolbarConfig:{
+      pin: true,
+      hide: false
+    }
+  })
+
   /**
    * 测试数据
    * @type {{time: string, title: string, content: string}}
@@ -39,8 +56,20 @@
     right: 16px;
     top: 8px;
     color: #91aefa;
+    z-index: 999;
   }
   .content-container .content-self{
     min-height: 90vh;
+    border: 0;
+    position: relative;
+  }
+  .content-container ::v-deep(.vditor-toolbar) {
+    padding: 0;
+    line-height: 0;
+    border-radius: 0;
+    border: 0;
+    border-bottom: 1px solid #eee;
+    background: linear-gradient(to right bottom, #edf0fc88, #faf3f988);
+    backdrop-filter: blur(4px);
   }
 </style>
